4290554fc42a0962fcd39a23f2ad3846126b2542
Lucas Marrane Siler <lucasmarrane@gmail.com>
2023-04-08 19:57:28 -0300

    jsdoc

--- src/validations/index.ts
+++ src/validations/index.ts
@@ -2,6 +2,11 @@ import * as Yup from "yup";
 
 type TSchemaFactory = (yup: typeof Yup) => Yup.Schema;
 
+/**
+ * Create a schema
+ * @param callbackSchema 
+ * @returns 
+ */
 export function schemaFactory(callbackSchema: TSchemaFactory) {
   return callbackSchema(Yup);
 }
